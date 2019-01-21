9e122010bdf82ed4c98cab056bfae6f7a7ef8091
zhan0903 <z.han0903@qq.com>
2019-01-21 16:17:58 +1100

    test obl-erl-p

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ class Parameters:
 original = False
 
 
-@ray.remote
+@ray.remote(num_gpus=2)
 class Worker(object):
     def __init__(self, args):
         # self.env = env_creator(config["env_config"]) # Initialize environment.
