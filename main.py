a9cdbe5901ba53a031fd8e9ee0ff77fe79fbfbf6
zhan0903 <z.han0903@qq.com>
2019-01-21 12:22:12 +1100

    first commit

---
+++ main.py
@@ -0,0 +1,233 @@
+import numpy as np, os, time, sys, random
+from core import mod_neuro_evo as utils_ne
+from core import mod_utils as utils
+import gym, torch
+from core import replay_memory
+from core import ddpg as ddpg
+import argparse
+import torch.multiprocessing as mp
+import time
+import logging
+import copy
+import ray
+
+
+render = False
+parser = argparse.ArgumentParser()
+parser.add_argument('-env', help='Environment Choices: (HalfCheetah-v2) (Ant-v2) (Reacher-v2) (Walker2d-v2) (Swimmer-v2) (Hopper-v2)', required=True)
+env_tag = vars(parser.parse_args())['env']
+
+
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+logger.setLevel(level=logging.DEBUG)
+
+
+class Parameters:
+    def __init__(self):
+        #Number of Frames to Run
+        if env_tag == 'Hopper-v2': self.num_frames = 4000000
+        elif env_tag == 'Ant-v2': self.num_frames = 6000000
+        elif env_tag == 'Walker2d-v2': self.num_frames = 8000000
+        else: self.num_frames = 2000000
+
+        #USE CUDA
+        self.is_cuda = True; self.is_memory_cuda = True
+
+        #Sunchronization Period
+        if env_tag == 'Hopper-v2' or env_tag == 'Ant-v2': self.synch_period = 1
+        else: self.synch_period = 10
+
+        #DDPG params
+        self.use_ln = True # True
+        self.gamma = 0.99; self.tau = 0.001
+        self.seed = 7
+        self.batch_size = 128
+        self.buffer_size = 1000000
+        self.frac_frames_train = 1.0
+        self.use_done_mask = True
+
+        ###### NeuroEvolution Params ########
+        #Num of trials
+        if env_tag == 'Hopper-v2' or env_tag == 'Reacher-v2': self.num_evals = 5
+        elif env_tag == 'Walker2d-v2': self.num_evals = 3
+        else: self.num_evals = 1
+
+        #Elitism Rate
+        if env_tag == 'Hopper-v2' or env_tag == 'Ant-v2': self.elite_fraction = 0.3
+        elif env_tag == 'Reacher-v2' or env_tag == 'Walker2d-v2': self.elite_fraction = 0.2
+        else: self.elite_fraction = 0.1
+
+        self.pop_size = 10
+        self.crossover_prob = 0.0
+        self.mutation_prob = 0.9
+
+        #Save Results
+        self.state_dim = None; self.action_dim = None #Simply instantiate them here, will be initialized later
+        self.save_foldername = 'test3-debug/%s/' % env_tag
+        if not os.path.exists(self.save_foldername): os.makedirs(self.save_foldername)
+
+original = False
+
+
+@ray.remote
+class Worker(object):
+    def __init__(self, args):
+        # self.env = env_creator(config["env_config"]) # Initialize environment.
+        # self.policy = ddpg.Actor(args)
+        self.env = utils.NormalizedActions(gym.make(env_tag))
+        # self.policy.eval()
+        self.replay_memory = replay_memory(args.buffer_size)
+        # Details omitted.
+
+    def add_experience(self, state, action, next_state, reward, done):
+        reward = utils.to_tensor(np.array([reward])).unsqueeze(0)
+        if self.args.is_cuda: reward = reward.cuda()
+        if self.args.use_done_mask:
+            done = utils.to_tensor(np.array([done]).astype('uint8')).unsqueeze(0)
+            if self.args.is_cuda: done = done.cuda()
+        action = utils.to_tensor(action)
+        if self.args.is_cuda: action = action.cuda()
+        self.replay_buffer.push(state, action, next_state, reward, done)
+
+    def evaluate(self, net, is_render, is_action_noise=False, store_transition=True):
+        total_reward = 0.0
+        net.eval()
+
+        state = self.env.reset()
+        state = utils.to_tensor(state).unsqueeze(0)
+        if self.args.is_cuda: state = state.cuda()
+        done = False
+
+        while not done:
+            if store_transition: self.num_frames += 1; self.gen_frames += 1
+            if render and is_render: self.env.render()
+            action = net.forward(state)
+            action.clamp(-1, 1)
+            action = utils.to_numpy(action.cpu())
+            if is_action_noise: action += self.ounoise.noise()
+
+            next_state, reward, done, info = self.env.step(action.flatten())  # Simulate one step in environment
+            next_state = utils.to_tensor(next_state).unsqueeze(0)
+            if self.args.is_cuda:
+                next_state = next_state.cuda()
+            total_reward += reward
+
+            if store_transition: self.add_experience(state, action, next_state, reward, done)
+            state = next_state
+        if store_transition: self.num_games += 1
+
+        return total_reward
+
+
+class Agent:
+    def __init__(self, args, env):
+        self.args = args; self.env = env
+        self.evolver = utils_ne.SSNE(self.args)
+        # experiences = replay_memory()
+
+        self.workders = [Worker.remote(args)
+           for _ in range(self.args.pop_size)]
+
+        self.num_games = 0; self.num_frames = 0; self.gen_frames = None
+
+    def list_argsort(self, seq):
+        return sorted(range(len(seq)), key=seq.__getitem__)
+
+    def rl_to_evo(self, rl_net, evo_net):
+        for target_param, param in zip(evo_net.parameters(), rl_net.parameters()):
+            target_param.data.copy_(param.data)
+
+    def train(self):
+        self.gen_frames = 0
+        print("begin training")
+
+        ####################### EVOLUTION #####################
+        all_fitness = []
+        #Evaluate genomes/individuals
+        # replay_buffer = replay_memory.ReplayMemory(self.args.buffer_size)
+        # experiences_id = ray.put(replay_buffer)
+        thetas = [ddpg.Actor(self.args) for _ in range(self.args.pop_size)]
+        theta_ids = ray.put(thetas)
+
+        evaluate_ids = [worker.evaluate.remote(theta_id) for worker, theta_id in zip(self.workders,theta_ids)]
+        results = ray.get(evaluate_ids)
+        # print("results:")
+        logger.debug("results:{}".format(results))
+        exit(0)
+
+
+        # workers = [Worker.remote(config, policy_params, env_name, noise_id)
+        #    for _ in range(self.args.pop_size)]
+
+        for net in self.pop:
+            fitness = 0.0
+            for eval in range(self.args.num_evals): fitness += self.evaluate(net, is_render=False, is_action_noise=False)
+            all_fitness.append(fitness/self.args.num_evals)
+
+        best_train_fitness = max(all_fitness)
+        worst_index = all_fitness.index(min(all_fitness))
+
+        #Validation test
+        champ_index = all_fitness.index(max(all_fitness))
+        test_score = 0.0
+        for eval in range(5): test_score += self.evaluate(self.pop[champ_index], is_render=True, is_action_noise=False, store_transition=False)/5.0
+
+        #NeuroEvolution's probabilistic selection and recombination step
+        elite_index = self.evolver.epoch(self.pop, all_fitness)
+
+
+        # print("ddpg time:", (time.time()-time_evolution)/3600)
+        return best_train_fitness, test_score, elite_index
+
+
+if __name__ == "__main__":
+    parameters = Parameters()  # Create the Parameters class
+    tracker = utils.Tracker(parameters, ['erl'], '_score.csv')  # Initiate tracker
+    frame_tracker = utils.Tracker(parameters, ['frame_erl'], '_score.csv')  # Initiate tracker
+    time_tracker = utils.Tracker(parameters, ['time_erl'], '_score.csv')
+
+    #Create Env
+    env = utils.NormalizedActions(gym.make(env_tag))
+    parameters.action_dim = env.action_space.shape[0]
+    parameters.state_dim = env.observation_space.shape[0]
+
+    #Seed
+    env.seed(parameters.seed);
+    torch.manual_seed(parameters.seed); np.random.seed(parameters.seed); random.seed(parameters.seed)
+
+    #Create Agent
+    agent = Agent(parameters, env)
+    print('Running', env_tag, ' State_dim:', parameters.state_dim, ' Action_dim:', parameters.action_dim)
+
+    next_save = 100; time_start = time.time()
+    while agent.num_frames <= parameters.num_frames:
+        best_train_fitness, erl_score, elite_index = agent.train()
+        print('#Games:', agent.num_games, '#Frames:', agent.num_frames, ' Epoch_Max:', '%.2f'%best_train_fitness if best_train_fitness != None else None, ' Test_Score:','%.2f'%erl_score if erl_score != None else None, ' Avg:','%.2f'%tracker.all_tracker[0][1], 'ENV '+env_tag)
+        print('RL Selection Rate: Elite/Selected/Discarded', '%.2f'%(agent.evolver.selection_stats['elite']/agent.evolver.selection_stats['total']),
+                                                             '%.2f' % (agent.evolver.selection_stats['selected'] / agent.evolver.selection_stats['total']),
+                                                              '%.2f' % (agent.evolver.selection_stats['discarded'] / agent.evolver.selection_stats['total']))
+
+        tracker.update([erl_score], agent.num_games)
+        frame_tracker.update([erl_score], agent.num_frames)
+        time_tracker.update([erl_score], time.time()-time_start)
+
+        #Save Policy
+        if agent.num_games > next_save:
+            next_save += 100
+            if elite_index != None: torch.save(agent.pop[elite_index].state_dict(), parameters.save_foldername + 'evo_net')
+            print("Progress Saved")
+
+
+
+
+
+
+
+
+
+
+
