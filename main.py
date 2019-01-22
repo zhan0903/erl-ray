e3f8765ffc47c23031ace8fcbf47f80b3f615862
zhan0903 <z.han0903@qq.com>
2019-01-22 16:51:41 +1100

    test obl-erl-p

--- main.py
+++ main.py
@@ -208,6 +208,9 @@ class Agent:
         # assert len(self.workers) == len(thetas)
         # theta_id = ray.put(ddpg.Actor(self.args).state_dict())
         # while True:
+        set_num_id = self.worker[0].set_gen_frames.remote(0)
+        set_num = ray.get(set_num_id)
+
         get_num_ids = [worker.get_gen_num.remote() for worker in self.workers]
         gen_nums = ray.get(get_num_ids)
         print("gen_nums:{0}".format(gen_nums))
