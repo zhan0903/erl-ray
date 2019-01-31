4243b1ce264c8b3b87b89dc27488c49526c21d38
zhan0903 <z.han0903@qq.com>
2019-01-31 21:22:45 +1100

    debug erl-ray

--- main.py
+++ main.py
@@ -142,7 +142,7 @@ class Worker(object):
         net.load_state_dict(model)
         for _ in range(num_evals):
             fitness += self._evaluate(net, is_action_noise=is_action_noise, store_transition=store_transition)
-        return fitness / num_evals, self.num_frames, self.gen_frames, self.num_games
+        return np.array(fitness / num_evals, self.num_frames, self.gen_frames, self.num_games)
 
     def _evaluate(self, net, is_render=False, is_action_noise=False, store_transition=True):
         total_reward = 0.0
