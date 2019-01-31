6c93f6cd923a4ae0c367f550ef5f976e3db73141
zhan0903 <z.han0903@qq.com>
2019-01-31 15:22:06 +1100

    debug erl-ray

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ class Parameters:
 original = False
 
 
-@ray.remote(num_gpus=0.2)
+@ray.remote(num_gpus=0.1)
 class Worker(object):
     def __init__(self, args):
         # self.env = env_creator(config["env_config"]) # Initialize environment.
