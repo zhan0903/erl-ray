8085b80ebcc08797084d6e017cb373486ab7b048
zhan0903 <z.han0903@qq.com>
2019-01-31 11:34:51 +1100

    debug erl-ray

--- test_bug.py
+++ test_bug.py
@@ -23,10 +23,9 @@ class Parameters:
 
 
 if __name__ == "__main__":
-    ray.init()
+    ray.init(num_gpus=1)
     args = Parameters()
     workers = [Worker.remote(args) for _ in range(args.pop_size)]
-    # for worker in workers: worker.set_gen_frames.remote(0)
     get_num_ids = [worker.get_gen_num.remote() for worker in workers]
     gen_nums = ray.get(get_num_ids)
     print(gen_nums)
